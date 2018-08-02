29b5a2ac7e0fd583a4bcd956b375978bbb61ac00
Saturnino Abril <saturnino.abril@gmail.com>
2018-08-02 22:47:20 +0800

    modify returned values

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const server = Hapi.server({
     host: 'localhost'
 });
 
+const responseTypes = [
+    'in_channel',
+    'ephemeral',
+];
+
+const postTypes = [
+    '',
+    'slack_attachment',
+    'system_generic',
+];
+
 server.route({
     method: ['PUT', 'POST'],
     path: '/{hook}',
@@ -14,11 +25,22 @@ server.route({
         const hook = request.params.hook ?
             encodeURIComponent(request.params.hook) :
             'hook';
+        const {payload} = request;
         console.log(`Payload on /${hook}:`);
-        console.log(request.payload);
+        console.log(payload);
         console.log(`Header on /${hook}:`);
         console.log(request.headers);
-        return `Processing the hook on /${hook}`;
+
+        const defaultPostType = postTypes[0];
+        const defaultResponseType = responseTypes[0];
+
+        return {
+            text: `#### Outgoing Webhook Response\n - token: "${payload.token}"\n - trigger_word: "${payload.trigger_word}"\n - text: "${payload.text}"\n - channel_name: "${payload.channel_name}"\n - team_domain: "${payload.team_domain}"\n`,
+            username: `${payload.user_name}`,
+            icon_url: 'https://docs.mattermost.com/_images/icon-76x76.png',
+            type: defaultPostType,
+            response_type: defaultResponseType
+        };
     }
 });
 
